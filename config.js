12235a26f03a2f01f76c3a76ded4b1db4fdfe92c
Arun Seehra <seehra.a@gmail.com>
2018-10-23 13:35:45 -0500

    Configure mongoose

--- config.js
+++ config.js
@@ -1,5 +1,10 @@
 'use strict';
 
+const localUri = process.env.NODE_ENV !== 'test'
+  ? 'mongodb://localhost:27017/noteful'
+  : 'mongodb://localhost:27017/noteful-test';
+
 module.exports = {
   PORT: process.env.PORT || 8080,
+  MONGODB_URI: process.env.MONGODB_URI || localUri,
 };
