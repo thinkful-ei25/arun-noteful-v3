95630d8499304ce2e67cf61cdc1775e15b1d5bc5
Arun Seehra <seehra.a@gmail.com>
2018-10-25 13:13:54 -0500

    Add folder seed data

---
+++ db/seed/folders.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const folders = [
+  {
+    _id: '111111111111111111111100',
+    name: 'Archive',
+  },
+  {
+    _id: '111111111111111111111101',
+    name: 'Drafts',
+  },
+  {
+    _id: '111111111111111111111102',
+    name: 'Personal',
+  },
+  {
+    _id: '111111111111111111111103',
+    name: 'Work',
+  },
+];
+
+module.exports = folders;
