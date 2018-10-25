06c1db98352b501a14f0460e091200a32629c745
Arun Seehra <seehra.a@gmail.com>
2018-10-25 14:08:56 -0500

    Fix deprecation warnings

--- config.js
+++ config.js
@@ -5,5 +5,9 @@ module.exports = {
   TEST_DATABASE_URL:
     process.env.TEST_DATABASE_URL || 'mongodb://localhost:27017/noteful-test',
   MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/noteful',
-  MONGODB_OPTIONS: { useNewUrlParser: true, useFindAndModify: false },
+  MONGODB_OPTIONS: {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  },
 };
